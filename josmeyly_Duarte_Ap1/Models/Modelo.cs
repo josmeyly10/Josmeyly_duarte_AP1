3cc1a77faed2fb3dc48d14ebb9fff84a7584a07c
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-02-03 20:18:25 -0400

    creacion del contexto

---
+++ josmeyly_Duarte_Ap1/Models/Modelo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace josmeyly_Duarte_Ap1.Models
+{
+    public class Modelo
+    {
+        [Key]
+        public int ModeloId { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten Letras")]
+        [Required(ErrorMessage = "Campo Obligatorio")]
+
+        public string? Nombres { get; set; }
+
+    }
+}
