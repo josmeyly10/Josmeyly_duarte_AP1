abc9a9d5bb4c93203b0116760818a70eacad7e1a
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-02-17 21:06:31 -0400

    se hicieron los modelos de aporte

---
+++ josmeyly_Duarte_Ap1/Models/Aporte.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace josmeyly_Duarte_Ap1.Models
+{
+    public class Aporte
+    {
+        [Key]
+        public int AporteId { get; set; }
+
+        public DateTime? Fecha {  get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage ="Campo obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage=" solo se permiten letras")]
+
+        public string? Persona { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = " solo se permiten letras")]
+        public string? Observacion{ get; set; } 
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+
+        public double? Monto { get; set; }
+
+        
+       
+
+       
+
+    }
+}
