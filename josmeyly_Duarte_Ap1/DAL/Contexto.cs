76df488365c1aba8059f48666f082edd5105c2fd
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-02-17 21:06:44 -0400

    se creo el contexto

--- josmeyly_Duarte_Ap1/DAL/Contexto.cs
+++ josmeyly_Duarte_Ap1/DAL/Contexto.cs
@@ -6,7 +6,9 @@ namespace josmeyly_Duarte_Ap1.DAL
    
         public class Contexto : DbContext
         {
-
+        public Contexto(DbContextOptions<Contexto> options):base (options) { }
+         
+         public DbSet<Aporte> Aporte { get; set; } //tabla del aporte 
            
         }
 }
